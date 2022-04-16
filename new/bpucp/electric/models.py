18fae03e8023e1908c05acf632a5fc8a3de408b8
Robinaf <robatemesgen42@gmail.com>
2022-04-16 23:01:31 +0300

    customer role final

--- new/bpucp/electric/models.py
+++ new/bpucp/electric/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-from users.models import Customer
-
-
-# Create your models here.
-
-class ElectricCustomer(models.Model):
-    meter_id=models.IntegerField(primary_key=True)
-    username = models.ForeignKey(null=True,max_length=100,on_delete=models.CASCADE, to=Customer)
-   
-    # def __str__(self):
-    #     return self.meter_id
-    class Meta:
-        db_table = "electric_customer"
